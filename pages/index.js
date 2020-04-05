fc7241c43c42ed1887255fad7e83b94c8985d35e
John Aldrich Bernardo <4ldrich@protonmail.com>
2020-04-05 06:27:38 -0400

    Adding head

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, Fragment } from "react";
 
 import { Box } from "../components";
 
@@ -9,9 +9,14 @@ import Head from 'next/head'
 
 export default function Index() {
   return (
-    <Box>
+    <Fragment>
+      <Head>
+        <title>Only YNews</title>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
       <Header title="YCombinator News" />
       <News />
-    </Box>
+    </Fragment>
   );
 }
